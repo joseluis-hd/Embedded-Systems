ffaf8943436c6b826e67ef76e92318a24d60e41f
José Luis Haro Díaz <jlharodiaz03@gmail.com>
2025-09-03 09:52:51 -0600

    Add practice03 serial communication examples
    Renamed Arduino and LabVIEW example files for consistency. Added new serial communication examples: Arduino/practice03_serial_comm.ino and LabVIEW/practice03_serial_comm.vi to demonstrate serial pin control and communication.

---
+++ Arduino/practice03_serial_comm/practice03_serial_comm.ino
@@ -0,0 +1,22 @@
+int pin;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  if Serial.available() > 0
+  {
+    pin = Serial.read();
+
+    pinMode(pin, OUTPUT);
+
+    digitalWrite(pin, HIGH);
+
+    delay(1000);
+
+    digitalWrite(pin, LOW);
+  }
+}
