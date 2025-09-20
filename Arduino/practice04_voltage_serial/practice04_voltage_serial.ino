54a842edcca12631df1c4de386e310c198134614
José Luis Haro Díaz <jlharodiaz03@gmail.com>
2025-09-20 15:49:02 -0600

    Add voltage reading via serial for Arduino and LabVIEW
    Implemented Arduino code to read analog voltage, convert it, and send it over serial. Added initial LabVIEW VI for serial voltage practice.

--- Arduino/practice04_voltage_serial/practice04_voltage_serial.ino
+++ Arduino/practice04_voltage_serial/practice04_voltage_serial.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+int bites;
+float voltaje;
 
+void setup() 
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  bites = analogRead(0);
 
+  voltaje = (bites) * (5.0 / 1023.0);
+
+  Serial.println(voltaje);
+
+  delay(25);
 }
