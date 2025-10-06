0f16b6029242d6ddffd3684eef9172eb5f240ee8
José Luis Haro Díaz <jlharodiaz03@gmail.com>
2025-10-05 23:18:41 -0600

    Add practice 6 arduino code

--- Arduino/practice06_led_control/practice06_led_control.ino
+++ Arduino/practice06_led_control/practice06_led_control.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+int pin;
+int lugar;
 
+int numdec;
+int digbin;
+
+void setup()
+{
+  Serial.begin(9600);
+  for(pin = 2; pin <= 9; pin++)
+  {
+    pinMode(pin, OUTPUT);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  if(Serial.available() > 0)
+  {
+    numdec = Serial.read();
+    for(lugar = 7; lugar >= 0; lugar--)
+    {
+      digbin = bitRead(numdec, lugar);
+      pin = lugar + 2;
+      digitalWrite(pin, digbin);
+      Serial.print(digbin);
+    }
+  }
 }
