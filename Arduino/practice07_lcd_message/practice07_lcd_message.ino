a17ce54971655c81cad98ecdcd430214834d8153
José Luis Haro Díaz <jlharodiaz03@gmail.com>
2025-10-27 11:22:38 -0600

    Update code

--- Arduino/practice07_lcd_message/practice07_lcd_message.ino
+++ Arduino/practice07_lcd_message/practice07_lcd_message.ino
@@ -1,30 +1,32 @@
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(2,3,4,5,6,7);
-char caracter;
-String palabra;
 
-void setup()
-{
-  lcd.begin(16,2);
-  lcd.setCursor(0,0);
-  Serial.begin(9600);
+LiquidCrystal lcd(2,11,4,5,6,7);
+
+char caracter;     
+String palabra = ""; 
+
+void setup() {
+  lcd.begin(16, 2);           
+  lcd.setCursor(0, 0);        
+  Serial.begin(9600);         
 }
 
-void loop()
-{
-  while(Serial.available()>0)
-  {
-    caracter = Serial.read();
-    if(caracter == '/')
-    {
-      lcd.clear();
-      break;
+void loop() {
+  while (Serial.available() > 0) {
+    caracter = Serial.read(); 
+
+    if (caracter == '/') {
+      lcd.clear();            
+      break;                  
     }
-    palabra.concat(caracter);
-    delay(1);
+
+    palabra.concat(caracter); 
+    delay(1);                
   }
-  Serial.print(palabra);
-  lcd.print(palabra);
-  delay(50);
-  palabra = "";
+
+  Serial.print(palabra);    
+  lcd.print(palabra);       
+  
+  delay(50);             
+  palabra = "";            
 }
